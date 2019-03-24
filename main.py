f1358110074c90863bc4da5eb22ac216c06caabf
aadame3311 <antonio.adame01@utrgv.edu>
2019-03-24 15:48:18 -0500

    ..

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from nami import *
 from tony import *
 
 if __name__ == "__main__":
-    RecordVideo('Tony-Capture', './Dataset/PT/')
+    RecordVideo('Tony0', './Dataset/PT/')
     #openCam()
     hello()
