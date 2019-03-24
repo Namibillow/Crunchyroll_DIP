73691c596a6b8a1060774c0866999f5eddc0e5dd
Nami Akazawa <akazawan73@gmail.com>
2019-03-24 15:36:33 -0500

    argparser

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from dan import *
 from nami import *
 from tony import *
 
+import argparse
+
+
 if __name__ == "__main__":
-    RecordVideo('Tony-Capture', './Dataset/PN/')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--name", required=True,
+                        help="name of the person you are recording")
+
+    args = vars(parser.parse_args())
+
+    RecordVideo(args['name'], './Dataset/PN/')
     # openCam()
