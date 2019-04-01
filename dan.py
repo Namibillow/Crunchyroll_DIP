f0e174f20a61ae4f65115c49584fa292b9622f37
Daniel Ruiz <r.daniel1788@gmail.com>
2019-04-01 18:24:20 -0500

    Added comments and removed .vscode
    Added the comments for how Im going to handle the input files for the creation of the videos into dot/dash
    Also comments for how to create 0-9 in morse code

--- dan.py
+++ dan.py
@@ -1,21 +1,25 @@
-import cv2
-
-def openCam():
-    cv2.namedWindow("preview")
-    cv2.namedWindow("preview")
-    vc = cv2.VideoCapture(0)
-
-    if vc.isOpened(): # try to get the first frame
-        rval, frame = vc.read()
-    else:
-        rval = False
-
-    while rval:
-        cv2.imshow("preview", frame)
-        rval, frame = vc.read()
-        key = cv2.waitKey(20)
-        if key == 27: # exit on ESC
-            break
-    vc.release()
-    cv2.destroyWindow("preview")
+'''
+    Need to create sequences for both '.' and '_'
+    def createDot(directory)
+    def createDash(directory)
+        where the directory passed is the one containing 3 frames 
+        for the creation (labeled closed.jpg, open.jpg, half.jpg) of the Dot and Dash.
+    def create nums(directory)
+        where the create dot and dash are in
+    Numbers in morse code:
+        '0':'-----',        '1':'.----',         '2':'..---',
+        '3':'...--',        '4':'....-',         '5':'.....',
+        '6':'-....',        '7':'--...',         '8':'---..',
+        '9':'----.', 
     
+    If we ever get to it alphabet in morse code:
+        'A':'.-',           'B':'-...',          'C':'-.-.',
+        'D':'-..',          'E':'.',             'F':'..-.',
+        'G':'--.',          'H':'....',          'I':'..',
+        'J':'.---',         'K':'-.-',           'L':'.-..',
+        'M':'--',           'N':'-.',            'O':'---',
+        'P':'.--.',         'Q':'--.-',          'R':'.-.',
+        'S':'...',          'T':'-',             'U':'..-', 
+        'V':'...-',         'W':'.--',           'X':'-..-', 
+        'Y':'-.--',         'Z':'--..',
+'''
