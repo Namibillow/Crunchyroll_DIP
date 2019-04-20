4e629a4860a87b07c55e0c6d9a81a337c8278d5b
Nami Akazawa <akazawan73@gmail.com>
2019-04-20 13:25:36 -0500

    ignore

--- model/net.py
+++ model/net.py
@@ -1,1 +1,13 @@
 '''define the model'''
+
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras import backend as K
+from keras.layers import LSTM, Bidirectional, TimeDistributed
+
+
+def s2c(depth=2):
+    print('Building the model...')
+
+    for dep in range(depth):
+        pass
